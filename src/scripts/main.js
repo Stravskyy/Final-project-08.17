481f5da2ac560428197a7d28285e6a509dde2fb4
Stravskyy <stravskyy@gmail.com>
2017-08-25 19:21:10 +0300

    sergice page, about us-acordeon finished

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,16 +1,31 @@
 $(document).ready(function(){
-    $('.bxslider').bxSlider({
+    $('#slider1').bxSlider({
         mode: 'fade',
-        auto: true,
+        auto:false,
+        pause: 6000,
+        pager: false,
+        adaptiveHeight: false,
+        nextText:'',
+        prevText:''
+
+    });
+});
+
+$(document).ready(function(){
+    $('#slider2').bxSlider({
+        mode: 'fade',
+        auto:false,
         pause: 6000,
         pager: false,
         adaptiveHeight: true,
         nextText:'',
         prevText:''
 
-
     });
+
 });
+
+;
 
 $(document).ready(function() {
     $("nav [href]").each(function() {
@@ -25,4 +40,7 @@ window.addEventListener("beforeunload", function () {
     document.body.classList.add("animate-out");
 });
 
+$('#about-list h5').click(function() {
+    $(this).find("i.fa").toggleClass("fa-chevron-down fa-chevron-up");
+});
 
